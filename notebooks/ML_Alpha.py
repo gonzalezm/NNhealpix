13bd30d7c3cb1ae8b73b140846aab5202f3cf6bd
AlleneChristopher <christopher.allene@etu.univ-grenoble-alpes.fr>
2019-06-27 09:32:41 +0200

    Few modifications

--- notebooks/ML_Alpha.py
+++ notebooks/ML_Alpha.py
@@ -9,13 +9,23 @@ import healpy as hp
 from keras.models import Sequential
 import nnhealpix as nn
 import nnhealpix.layers
-import keras.backend as K
 import sys
+import os
+import datetime
 
-# Récupération des données
-directory = sys.argv[0]
-l_p = np.load(directory + "l_p.npy")
-Maps = np.load(directory + "Maps.npy")
+# Directory selection
+dir = sys.argv[1]
+today = datetime.datetime.now().strftime('%Y%m%d')
+out_dir = dir + '{}/'.format(today)
+
+try:
+    os.makedirs(out_dir)
+except:
+    pass
+
+# Take the data
+l_p = np.load(out_dir + "/l_p.npy")
+Maps = np.load(out_dir + "/Maps.npy")
 
 nside = 16
 ecart_Maps = np.sqrt(np.var(Maps))
@@ -23,17 +33,17 @@ if Maps.shape != (len(l_p),12*nside**2) :
     print("Erreur: Les maps n'ont pas la bonne shape ", Maps.shape, "au lieu de ", (len(l_p),12*nside**2))
 print("\n A titre indicatif l'écart type des valeur des Maps :", ecart_Maps)
 
-#Préparation des données pour le Machine Learning
+#Data preprocessing Machine Learning
 Ntest = 0.01*len(l_p)
 Ntrain = len(l_p)-Ntest
-# Attribution des valeurs d'entrées et de sorties pour l'entrainement et les tests
+# Split between train and test
 X_train = Maps[:, 0:(Ntrain)]
 y_train = l_p[0: (Ntrain)]
 X_test = Maps[:, (Ntrain):(Ntrain + Ntest)]
 y_test = l_p[(Ntrain) : (Ntrain + Ntest)]
 
 num_classes = 1
-# Mise en forme adequate
+# Changing the shape for NBB
 X_train = X_train.T
 X_test = X_test.T
 X_train = X_train.reshape(X_train.shape[0], len(X_train[0]), 1)
@@ -42,22 +52,22 @@ shape=(len(Maps[:,0]), 1)
 print("\n Les shapes des $X_train$ et des $X_test$ ainsi que celle de l'input attendu: ", X_train.shape, shape, X_test.shape)
 print("\n Les shapes des $y_train$ et des $y_test$: ",y_train.shape, y_test.shape)
 
-# NN avec une boucle NBB
+# NN with NBB loop
 inputs=kr.layers.Input(shape)
 x=inputs
 
-########### Boucle NBB #################################
+# NBB loop
 
 for i in range (int(math.log(nside,2))):
-#Recog of the neighbours & Convolution
+    #Recog of the neighbours & Convolution
     print(int(nside/(2**(i))), int(nside/(2**(i+1))))
     x = nnhealpix.layers.ConvNeighbours(int(nside/(2**(i))), filters=32, kernel_size=9)(x)
     x = kr.layers.Activation('relu')(x)
-#Degrade
+    #Degrade
     x = nnhealpix.layers.MaxPooling(int(nside/(2**(i))), int(nside/(2**(i+1))))(x)
-       
-########## Sortie des NBB #############################
-     
+
+# End of the NBBs
+
 x = kr.layers.Dropout(0.2)(x)
 x = kr.layers.Flatten()(x)
 x = kr.layers.Dense(48)(x)
@@ -66,19 +76,19 @@ x = kr.layers.Dense(1)(x)
 
 out=kr.layers.Activation('relu')(x)
 
-# Création et mise en place du model
+# Creation of the model
 model = kr.models.Model(inputs=inputs, outputs=out)
 model.compile(loss=kr.losses.mse, optimizer='adam', metrics=[kr.metrics.mean_absolute_percentage_error])
 model.summary()
 
-# Entrainement du model
+# Model training
 hist = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split = 0.1, verbose = 1, shuffle = True)
 
-# Prediction sur 100 l_p
+# Prediction on 100 l_p
 prediction = model.predict(X_test)
 diff = prediction-y_test
 
-np.save(directory + "prediction",prediction)
-np.save(directory + "diff", diff)
-np.save(directory + "hist_loss",hist.history['loss'])
-np.save(directory + "hist_val_loss",hist.history['val_loss'])
+np.save(out_dir + "/prediction",prediction)
+np.save(out_dir + "/diff", diff)
+np.save(out_dir + "/hist_loss",hist.history['loss'])
+np.save(out_dir + "/hist_val_loss",hist.history['val_loss'])
