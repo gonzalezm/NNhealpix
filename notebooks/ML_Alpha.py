4a90d911fef3d833da64421be919da5cc23d43d1
Louise Mousset <mousset@apc.in2p3.fr>
2019-06-27 11:18:42 +0200

    small modif

--- notebooks/ML_Alpha.py
+++ notebooks/ML_Alpha.py
@@ -14,9 +14,10 @@ import os
 import datetime
 
 # Directory selection
-dir = sys.argv[1]
+map_dir = sys.argv[1]
+out_dir = sys.argv[2]
 today = datetime.datetime.now().strftime('%Y%m%d')
-out_dir = dir + '{}/'.format(today)
+out_dir += '{}/'.format(today)
 
 try:
     os.makedirs(out_dir)
@@ -24,23 +25,23 @@ except:
     pass
 
 # Take the data
-l_p = np.load(out_dir + "/l_p.npy")
-Maps = np.load(out_dir + "/Maps.npy")
+l_p = np.load(map_dir + "/l_p.npy")
+Maps = np.load(map_dir + "/Maps.npy")
 
 nside = 16
 ecart_Maps = np.sqrt(np.var(Maps))
-if Maps.shape != (len(l_p),12*nside**2) :
-    print("Erreur: Les maps n'ont pas la bonne shape ", Maps.shape, "au lieu de ", (len(l_p),12*nside**2))
+if Maps.shape != (len(l_p), 12 * nside ** 2):
+    print("Erreur: Les maps n'ont pas la bonne shape ", Maps.shape, "au lieu de ", (len(l_p), 12 * nside ** 2))
 print("\n A titre indicatif l'écart type des valeur des Maps :", ecart_Maps)
 
-#Data preprocessing Machine Learning
-Ntest = 0.01*len(l_p)
-Ntrain = len(l_p)-Ntest
+# Data preprocessing Machine Learning
+Ntest = 0.01 * len(l_p)
+Ntrain = len(l_p) - Ntest
 # Split between train and test
 X_train = Maps[:, 0:(Ntrain)]
 y_train = l_p[0: (Ntrain)]
 X_test = Maps[:, (Ntrain):(Ntrain + Ntest)]
-y_test = l_p[(Ntrain) : (Ntrain + Ntest)]
+y_test = l_p[(Ntrain): (Ntrain + Ntest)]
 
 num_classes = 1
 # Changing the shape for NBB
@@ -48,23 +49,24 @@ X_train = X_train.T
 X_test = X_test.T
 X_train = X_train.reshape(X_train.shape[0], len(X_train[0]), 1)
 X_test = X_test.reshape(X_test.shape[0], len(X_test[0]), 1)
-shape=(len(Maps[:,0]), 1)
-print("\n Les shapes des $X_train$ et des $X_test$ ainsi que celle de l'input attendu: ", X_train.shape, shape, X_test.shape)
-print("\n Les shapes des $y_train$ et des $y_test$: ",y_train.shape, y_test.shape)
+shape = (len(Maps[:, 0]), 1)
+print("\n Les shapes des $X_train$ et des $X_test$ ainsi que celle de l'input attendu: ", X_train.shape, shape,
+      X_test.shape)
+print("\n Les shapes des $y_train$ et des $y_test$: ", y_train.shape, y_test.shape)
 
 # NN with NBB loop
-inputs=kr.layers.Input(shape)
-x=inputs
+inputs = kr.layers.Input(shape)
+x = inputs
 
 # NBB loop
 
-for i in range (int(math.log(nside,2))):
-    #Recog of the neighbours & Convolution
-    print(int(nside/(2**(i))), int(nside/(2**(i+1))))
-    x = nnhealpix.layers.ConvNeighbours(int(nside/(2**(i))), filters=32, kernel_size=9)(x)
+for i in range(int(math.log(nside, 2))):
+    # Recog of the neighbours & Convolution
+    print(int(nside / (2 ** (i))), int(nside / (2 ** (i + 1))))
+    x = nnhealpix.layers.ConvNeighbours(int(nside / (2 ** (i))), filters=32, kernel_size=9)(x)
     x = kr.layers.Activation('relu')(x)
-    #Degrade
-    x = nnhealpix.layers.MaxPooling(int(nside/(2**(i))), int(nside/(2**(i+1))))(x)
+    # Degrade
+    x = nnhealpix.layers.MaxPooling(int(nside / (2 ** (i))), int(nside / (2 ** (i + 1))))(x)
 
 # End of the NBBs
 
@@ -74,7 +76,7 @@ x = kr.layers.Dense(48)(x)
 x = kr.layers.Activation('relu')(x)
 x = kr.layers.Dense(1)(x)
 
-out=kr.layers.Activation('relu')(x)
+out = kr.layers.Activation('relu')(x)
 
 # Creation of the model
 model = kr.models.Model(inputs=inputs, outputs=out)
@@ -82,13 +84,13 @@ model.compile(loss=kr.losses.mse, optimizer='adam', metrics=[kr.metrics.mean_abs
 model.summary()
 
 # Model training
-hist = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split = 0.1, verbose = 1, shuffle = True)
+hist = model.fit(X_train, y_train, epochs=100, batch_size=32, validation_split=0.1, verbose=1, shuffle=True)
 
 # Prediction on 100 l_p
 prediction = model.predict(X_test)
-diff = prediction-y_test
+diff = prediction - y_test
 
-np.save(out_dir + "/prediction",prediction)
+np.save(out_dir + "/prediction", prediction)
 np.save(out_dir + "/diff", diff)
-np.save(out_dir + "/hist_loss",hist.history['loss'])
-np.save(out_dir + "/hist_val_loss",hist.history['val_loss'])
+np.save(out_dir + "/hist_loss", hist.history['loss'])
+np.save(out_dir + "/hist_val_loss", hist.history['val_loss'])
