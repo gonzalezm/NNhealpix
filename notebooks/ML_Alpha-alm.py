06ca0f4229a0af77143e412a2f5b0762b229a4e2
AlleneChristopher <christopher.allene@etu.univ-grenoble-alpes.fr>
2019-07-03 10:34:34 +0200

    Random model

--- notebooks/ML_Alpha-alm.py
+++ notebooks/ML_Alpha-alm.py
@@ -1,20 +1,14 @@
-import camb
-from camb import model, initialpower
-import tensorflow as tf
 import math
 import keras as kr
 import numpy as np
-from keras.models import Sequential
-import nnhealpix as nn
 import nnhealpix.layers
-from pylab import *
 import sys
 import os
 import datetime
 from joblib import dump
 
 # Directory where files will be saved
-map_dir = sys.argv[1]
+in_dir = sys.argv[1]
 dir = sys.argv[2]
 now = datetime.datetime.now().strftime('%Y%m%d_%H_%M_%S')
 out_dir = dir + '{}/'.format(now)
@@ -25,23 +19,28 @@ except:
     pass
 
 #Take data
-mymaps = np.load(map_dir + "/mymaps.npy")
-myclsmod = np.load(map_dir + "/myclsmod.npy")
+mymaps = np.load(in_dir + "/mymaps.npy")
+mycls = np.load(in_dir + "/mycls.npy")
 ns = np.sqrt(mymaps.shape[1]/12)
 print("Nside = ", ns)
+
+#Take random data
+map_rdm = np.load(in_dir + "/map_rdm.npy")
+cls_rdm = np.load(in_dir + "/cls_rdm.npy")
 
 #Machine learning
 #Data preprocess  
 
 print("The shape of mymaps (inputs): ", mymaps.shape)
-print("The shape of mycls / myclsmod (outputs): ", myclsmod.shape)
-num_out = myclsmod.shape[1]
+print("The shape of mycls / myclsmod (outputs): ", mycls.shape)
+num_out = mycls.shape[1]
 shape=(len(mymaps[0,:]),1)
 mymaps = mymaps.reshape(mymaps.shape[0], len(mymaps[0]), 1)
 print("The shape of mymaps (inputs) after reshape: ", mymaps.shape)
 nbmodels = mymaps.shape[0]
 print("Number of model: ", nbmodels)
 nbtest = int(0.1*nbmodels)
+map_rdm = map_rdm.reshape(map_rdm.shape[0], len(map_rdm[0]), 1)
 
 #NBB layers
 inputs=kr.layers.Input(shape)
@@ -68,13 +67,31 @@ mymodel.compile(loss=kr.losses.mse, optimizer='adam', metrics=[kr.metrics.mean_a
 mymodel.summary()
 
 # Training
-hist = mymodel.fit(mymaps[:(nbmodels-nbtest),:,:], myclsmod[:(nbmodels-nbtest),:], epochs=100, batch_size=32, validation_split = 0.1, verbose = 1, shuffle = True)
+hist = mymodel.fit(mymaps[:(nbmodels-nbtest),:,:], mycls[:(nbmodels-nbtest),:], epochs=15, batch_size=32, validation_split = 0.1, verbose = 1, shuffle = True)
+
+#Random model
+model_rdm = kr.models.Model(inputs=inputs, outputs=out)
+model_rdm.compile(loss=kr.losses.mse, optimizer='adam', metrics=[kr.metrics.mean_absolute_percentage_error])
+model_rdm.summary()
+
+#random training
+hist_rdm=model_rdm.fit(map_rdm[:(nbmodels-nbtest),:,:], cls_rdm[:(nbmodels-nbtest),:], epochs=15, batch_size=32, validation_split = 0.1, verbose = 1, shuffle = True)
 
 #Make a prédiction
-prediction=mymodel.predict(mymaps[(nbmodels-nbtest):,:,:])
+prediction = mymodel.predict(mymaps[(nbmodels-nbtest):,:,:])
+prediction2 = mymodel.predict(map_rdm[(nbmodels-nbtest):,:,:])
+
+#Prediction on random model with random and smooth
+pred_rdm = model_rdm.predict(mymaps[(nbmodels-nbtest):,:,:])
+pred_rdm2 = model_rdm.predict(map_rdm[(nbmodels-nbtest):,:,:])
 
 #Saves
 dump(mymodel, out_dir + "/mymodel.joblib")
 np.save(out_dir + "/prediction", prediction)
+np.save(out_dir + "/prediction2", prediction2)
+np.save(out_dir + "/pred_rdm", pred_rdm)
+np.save(out_dir + "/pred_rdm2", pred_rdm2)
 np.save(out_dir + "/loss-alm", hist.history['loss'])
 np.save(out_dir + "/val_loss-alm", hist.history['val_loss'])
+np.save(out_dir + "/loss-alm_rdm", hist_rdm.history['loss'])
+np.save(out_dir + "/val_loss-alm_rdm", hist_rdm.history['val_loss'])
