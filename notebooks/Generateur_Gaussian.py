1b568dfa6ebe7aa3b0d5b209408744de19eac66b
AlleneChristopher <christopher.allene@etu.univ-grenoble-alpes.fr>
2019-07-12 09:21:42 +0200

    Cl between 0 and 40

--- notebooks/Generateur_Gaussian.py
+++ notebooks/Generateur_Gaussian.py
@@ -26,14 +26,14 @@ except:
     pass
 
 # Number of map you want
-nmap = np.int(sys.argv[3])
+nmap = sys.argv[3]
 
 nside = 16
 
 # Create random gaussian mean for the spectra
 # between 5 and 50
 l_p = 15 * np.random.random_sample(nmap,) + 5
-#noise = np.random.randn(12*nside**2,nmap)*float(sys.argv[4])
+
 #Take some informations about the l_p
 moy_l_p = np.mean(l_p)
 ecart_l_p = np.std(l_p)
@@ -44,14 +44,13 @@ print('l_p mean, std and max : {0} {1} {2}'.format(moy_l_p, ecart_l_p, max_lp))
 
 # Create spectra (Cl) and associated maps
 sigma_p = 5.0
-l = np.linspace(5.0, 100.0, 2000)
+l = np.linspace(0.0, 40.0, 2000)
 print('l shape = {}'.format(l.shape))
-C_l = np.empty((len(l), len(l_p)))
-Maps = np.empty((12 * nside ** 2, len(l_p)))
+C_l = np.empty((len(l), nmap))
+Maps = np.empty((12 * nside ** 2, nmap))
 for j, lp in enumerate(l_p):
     C_l[:, j] = stats.norm.pdf(l, lp, sigma_p) + 10.**(-5)
     Maps[:, j] = hp.sphtfunc.synfast(C_l[:, j], nside, verbose = 0)
-Maps=Maps #+noise
 # Save lp, Cl, maps in 3 files
 np.save(out_dir + name + '_l_p', l_p)
 np.save(out_dir + name + '_C_l', C_l)
