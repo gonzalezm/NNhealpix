7d58ff1beef6b508f6c9c1995c73be544926f3b4
Louise Mousset <mousset@apc.in2p3.fr>
2019-09-10 10:08:48 -0300

    merge 2 scripts that were very similar

--- scripts/ML_Cl_final.py
+++ scripts/ML_Cl_final.py
@@ -7,31 +7,19 @@ import datetime
 
 import ConvNNTempLib as cnn
 
-# Take in arguments path where find the data and path where save new data.
-# Take the name given during the creation of data
-name_in = sys.argv[1]
-in_dir = sys.argv[2]
-out_dir = sys.argv[3]
+
+# This program is designed to find complete TT spectrum from CMB maps
+# The goal of this is to compare ML to anafast function for smooth data and random data
+# It saves data from the training, predictions done by the model and the model itself
+
+# Arguments
+name_in = sys.argv[1] # Name given to data you want to load
+in_dir = sys.argv[2] # Directory path for loading data
+out_dir = sys.argv[3] # Directory path for saving results
 
 # Add date and time to the path to save to avoid "same name file" problems.
 today = datetime.datetime.now().strftime('%Y%m%d_%H_%M_%S')
 out_dir += '/{}/'.format(today)
-
-# Load the data
-C_l = np.load(in_dir + "/" + name_in + "_C_l.npy")
-Maps = np.load(in_dir + "/" + name_in + "_Maps.npy")
-
-Ntest = 0.1
-Ntrain = 1 - Ntest
-sigma_n = sys.argv[4]
-Nside = hp.npix2nside(Maps.shape[1])
-
-Maps = cnn.AddWhiteNoise(Maps, sigma_n)
-Maps = cnn.NormalizeMaps(Maps)
-
-X_train, y_train, X_test, y_test, num_out, shape = cnn.PreprocessML(Maps, C_l, Ntest, Ntrain)
-
-inputs, out = cnn.ConvNNhealpix(shape, Nside, num_out)
 
 # Creat the repository where save new data
 try:
@@ -39,6 +27,27 @@ try:
 except:
     pass
 
+# Load the data
+C_l = np.load(in_dir + "/" + name_in + "_C_l.npy")
+Maps = np.load(in_dir + "/" + name_in + "_Maps.npy")
+
+# Get Nside
+Nside = hp.npix2nside(Maps.shape[1])
+
+# Add noise and normalize the maps
+sigma_n = 0.
+Maps = cnn.AddWhiteNoise(Maps, sigma_n)
+Maps = cnn.NormalizeMaps(Maps)
+
+# Split train/test data
+Ntest = 0.1
+Ntrain = 1 - Ntest
+X_train, y_train, X_test, y_test, num_out, shape = cnn.PreprocessML(Maps, C_l, Ntest, Ntrain)
+
+# Build all layers as it is in the paper
+inputs, out = cnn.ConvNNhealpix(shape, Nside, num_out)
+
+# Creation of the model and training
 model, hist, loss, val_loss = cnn.MakeAndTrainModel(X_train, y_train,
                                                     X_test, y_test,
                                                     epoch=20, batch_size=32,
@@ -46,13 +55,14 @@ model, hist, loss, val_loss = cnn.MakeAndTrainModel(X_train, y_train,
                                                     inputs=inputs, out=out,
                                                     retrain=False)
 
+# Print the final error
 error = model.evaluate(X_test, y_test)
 print('error :', error)
 
 # Save the model as a pickle in a file
 kr.models.save_model(model, out_dir + today + '_model.h5py.File')
 
-# np.save(out_dir + today + '_prediction', prediction)
+# Save results
 np.save(out_dir + today + '_hist_loss', hist.history['loss'])
 np.save(out_dir + today + '_hist_val_loss', hist.history['val_loss'])
 np.save(out_dir + today + '_history', hist.history)
