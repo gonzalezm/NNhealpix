9065cc761c3fc2ace7284526836c3d3e94c73f8b
Louise Mousset <lmousset@cca010.in2p3.fr>
2019-09-13 22:13:35 +0200

    test load model

--- scripts/training_test.py
+++ scripts/training_test.py
@@ -38,7 +38,10 @@ maps = cnn.NormalizeMaps(maps)
 X_train, X_test, y_train, y_test = skmodel.train_test_split(maps, lp, test_size=0.1)
 
 # Make a model
-model = cnn.make_model(nside, y_train[0].size)
+# premodel = '/sps/hep/qubic/Users/lmousset/Machine_learning/simulations/output-20190913194551/20190913194551_model.json'
+# weights = '/sps/hep/qubic/Users/lmousset/Machine_learning/simulations/output-20190913194551/20190913194551_weights.hdf5'
+# model = cnn.load_model(premodel, weights=None)
+model = cnn.make_model(nside, y_train[0].size, out_dir, today)
 
 # Train the model
 model, hist = cnn.make_training(model, X_train, y_train, 0.1, 10, 20, out_dir, today=today)
